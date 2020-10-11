734f1f258c9198d657b47ba1e658182f96055098
Nattida Wong <nattidac.wong@gmail.com>
2020-10-11 03:39:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,10 @@
 class CashRegister
 
-  attr_accessor :total
+  attr_accessor :total, :discount
 
-  def initialize(total = 0)
-    @total = total
+  def initialize(discount = 20)
+    @total = 0
+    @discount = discount
   end
 
 end
